6f04441f627f078ef7617af7f019097041f2011c
Ardha Iyan <emiliacomp.01@gmail.com>
2025-05-25 18:19:45 +0700

    Update generics.ts

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -199,11 +199,11 @@ export const generateMessageIDV2 = (userId?: string): string => {
 	random.copy(data, 28)
 
 	const hash = createHash('sha256').update(data).digest()
-	return 'EMICOMP' + hash.toString('hex').toUpperCase().substring(0, 18)
+	return 'GRCP' + hash.toString('hex').toUpperCase().substring(0, 18)
 }
 
 // generate a random ID to attach to a message
-export const generateMessageID = () => 'EMICOMP' + randomBytes(18).toString('hex').toUpperCase()
+export const generateMessageID = () => 'GRCP' + randomBytes(18).toString('hex').toUpperCase()
 
 export function bindWaitForEvent<T extends keyof BaileysEventMap>(ev: BaileysEventEmitter, event: T) {
 	return async(check: (u: BaileysEventMap[T]) => Promise<boolean | undefined>, timeoutMs?: number) => {
